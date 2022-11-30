e1470dcb274934e8c4dd76e1b922c4c72dc5f19c
ntnrgtic <77885151+ntnrgtic@users.noreply.github.com>
2022-12-01 03:48:02 +0530

    Add files via upload

--- TestModelv2/TestModelv1_init.m
+++ TestModelv2/TestModelv1_init.m
@@ -14,7 +14,7 @@ J = m*h^2; % Jxx
 D = m*a*h; % Jxz
 
 % simulation ending time
-t_end = 20;
+t_end = 8;
 
 mr = m_rider;
 hr = 0.7; % z position of rider's COM (tentatively)
@@ -27,32 +27,40 @@ lean_gain = -0.1*0;
 
 % intial values
 phi0 = 5*pi/180;
-V = 2; % velocity in m/s
+V = 10; % velocity in m/s
 
 k1 = b^2/(V^2*sin(lambda)-b*g*cos(lambda))/(m*a*c*sin(lambda));
 k2 = b*g/(V^2*sin(lambda)-b*g*cos(lambda));
 
-V_values = 0:;
+V_values = [3 15];
 
 global_t = [];
 global_phi = [];
 global_v = [];
 
 for i = 1:length(V_values)
-    delay = V_values(i);
+    V = V_values(i);
     k1 = b^2/(V^2*sin(lambda)-b*g*cos(lambda))/(m*a*c*sin(lambda));
     k2 = (b*g)/(V^2*sin(lambda)-b*g*cos(lambda));
     sim('bicycle_model_int.slx');
     phiSignal = ans.yout.getElement('phi');
+    phiDotSignal = ans.yout.getElement('phi_dot');
     t = phiSignal.Values.Time;
     phi = phiSignal.Values.Data;
+    phiDot = phiDotSignal.Values.Data;
 %     global_t = [global_t t'];
 %     global_phi = [global_phi phi'];
 %     global_v = [global_v ones(1,length(phi))*V];
-    plot3(t',ones(1,length(t'))*lean_gain,phi');
+%     annotation('arrow',phi, phiDot);
+    plot(phi, phiDot);
+    title('Phase Plot')
+    leg = legend(string(V_values));
+    title(leg, 'Velocity (m/s)');
 %     zlim([0 100]);
     hold on;
-    xlabel('time');
-    ylabel('lean gain');
-    zlabel('phi');
+    xlabel('phi');
+    ylabel('phi dot');
 end
+
+xline(0,'--k')
+yline(0,'--k')
